c278a114d6c4a8549269fd1d26c0a691d912ede7
Albion Morina <94832146+Albion007@users.noreply.github.com>
2021-12-16 20:12:25 +0100

    entire portfolio

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -22,6 +22,14 @@ export default function NavBar() {
                     activeClassName="text-red-300 bg-gray-700">
                         Projektet
                     </NavLink>
+                    <NavLink to="/trajnimet" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-yellow-800"
+                    activeClassName="text-red-300 bg-gray-700">
+                        Trajnimet
+                    </NavLink>
+                    <NavLink to="/konferencat" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-yellow-800"
+                    activeClassName="text-red-300 bg-gray-700">
+                        Konferencat
+                    </NavLink>
                     <NavLink to="/about" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-yellow-800"
                     activeClassName="text-red-300 bg-gray-700">
                         Rreth meje!
