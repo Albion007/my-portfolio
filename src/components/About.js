f698cc98345e9491c0c15e10d3894f81e2bd6515
Albion Morina <94832146+Albion007@users.noreply.github.com>
2021-12-16 22:33:14 +0100

    entire portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -30,7 +30,7 @@ export default function About() {
     return (
         <main className="relative">
             <img src={wp} alt="wp background" className="absolute w-full" />
-            <div className="p-10 lg:pt-48 container mx-auto relative">
+            <div className="p-10 lg:pt-40 container mx-auto relative">
                 <section className="bg-gray-500 rounded-lg shadow-2xl lg:flex p-20">
                     <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt={author.name} />
                     <div className="text-lg flex flex-col justify-center">
